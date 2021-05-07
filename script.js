b893cf943436b4a99cd1220c69407aaec45e10a0
github2k20 <priyansh.44rastogi@gmail.com>
2021-05-07 22:10:59 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/summary', async (req, res) => {
+
+    try {
+        const result = await axios.post(
+          "https://api.thegraph.com/subgraphs/name/vishalkale151071/cotrader",
+          {
+            query: `{
+                pairs{
+                    id
+                    token0{
+                      symbol
+                      totalSupply
+                      tradeVolume
+                    }
+                    token1{
+                      symbol
+                      totalSupply
+                      tradeVolume
+                    }
+                    token0Price
+                    token1Price
+                    volumeToken0
+                    volumeToken1
+                    totalSupply
+                  }
+              }`,
+          }
+        );
+        //console.log(result.data.data);
+        res.send(result.data.data)
+      } catch (e) {
+        console.log(e);
+      }
+  
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
+
+
+
