cb7448d1dcf711e81f707ec38550e2a79b5684ce
github2k20 <priyansh.44rastogi@gmail.com>
2021-05-08 10:30:30 +0530

    Made changes to deploy to heroku

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/summary', async (req, res) => {
+app.get('/', async (req, res) => {
 
     try {
         const result = await axios.post(
